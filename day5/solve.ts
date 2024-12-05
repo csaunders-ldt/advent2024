15a9d1fffbbd4caa41ece495ee0ea66b23083a82
Chris Saunders <cjasaunders@outlook.com>
2024-12-05 22:11:08 +0000

    feat(day): day 5

--- day5/solve.ts
+++ day5/solve.ts
@@ -1,39 +1,25 @@
-import { sortBy } from 'lodash';
+import { floor, partition, sum } from 'lodash';
 import { solve } from '../utils/typescript';
 
-type Input = {
-  maps: Record<string, string[]>;
-  pages: string[][];
-  sortedPages: string[][];
-};
+type Input = [unsorted: string[][], sorted: string[][]];
 
 function parser(input: string): Input {
   const [mapLines, pageLines] = input.split('\n\n');
-  const maps: Record<string, string[]> = {};
-  mapLines.split('\n').forEach((l) => {
-    const [k, v] = l.split('|');
-    (maps[k] ??= []).push(v);
-  });
+  const pairs = new Set(mapLines.split('\n'));
   const pages = pageLines.split('\n').map((l) => l.split(','));
 
   const sortedPages = pages.map((page) =>
-    page.sort((a, b) => {
-      if (maps[a]?.includes(b)) return -1;
-      if (maps[b]?.includes(a)) return 1;
-      return 0;
-    }),
+    page.sort((a, b) => (pairs.has(`${a}|${b}`) ? -1 : 1)),
   );
-
-  return { maps, pages, sortedPages };
+  return partition(sortedPages, (p) => input.includes(p.join(',')));
 }
 
-function part1(input: Input) {
-  console.log(input.sortedPages);
-  return -1;
+function part1([unsorted, _]: Input) {
+  return sum(unsorted.map((l) => +l[floor(l.length / 2)]));
 }
 
-function part2(input: Input) {
-  return -1;
+function part2([_, sorted]: Input) {
+  return sum(sorted.map((l) => +l[floor(l.length / 2)]));
 }
 
-solve({ part1, test1: 143, part2, test2: 5, parser });
+solve({ part1, test1: 143, part2, test2: 123, parser });
